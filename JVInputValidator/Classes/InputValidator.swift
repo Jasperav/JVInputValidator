abda7bfc5bd0d279ac99c4510d19f648b00a69f5
Jasper <Jasperav@hotmail.com>
2019-02-15 22:48:49 +0100

    commit

--- JVInputValidator/Classes/InputValidator.swift
+++ JVInputValidator/Classes/InputValidator.swift
@@ -1,15 +1,27 @@
 public enum ValidationState {
     case valid, invalid
+}
+
+public struct InputValidator: InputValidateable {
+    public private (set) var validationState: ValidationState
+    public var changedValidationState: ((ValidationState) -> ())!
+    
+    public init(validationState: ValidationState) {
+        self.validationState = validationState
+    }
     
     public mutating func update(isValid: Bool) {
         if isValid {
-            self = .valid
+            validationState = .valid
         } else {
-            self = .invalid
+            validationState = .invalid
         }
+        
+        changedValidationState(validationState)
     }
 }
 
-public protocol InputValidator {
+public protocol InputValidateable {
     var validationState: ValidationState { get }
+    var changedValidationState: ((ValidationState) -> ())! { get }
 }
